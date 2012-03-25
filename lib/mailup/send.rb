aa59797823d25f99c7e6e5bd1dd18e9bbac1b6e9
Brian Getting <brian@terra-firma-design.com>
2012-03-25 11:36:30 -0700

    Working on login

--- lib/mailup/send.rb
+++ lib/mailup/send.rb
@@ -8,7 +8,7 @@ module MailUp
       @client = Savon::Client.new do
         wsdl.document = 'https://wsvc.ss.mailup.it/MailupSend.asmx?WSDL'
       end
-      call(login_from_id, {:user => username, :pwd => password, :consoleId => username.gsub(/[a-z]/, '').to_i})
+      @access_key = call(:login_from_id, {:user => username, :pwd => password, :consoleId => username.gsub(/[a-z]/, '').to_i})
     end
     
   end
